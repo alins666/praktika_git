0145da55e2325371a8c4d470d7f7ee9eaa213907
alins666 <alinkhak23@gmail.com>
2025-05-31 10:55:07 +0700

    main.py и выбор

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+    inp = input("Выберите функцию: 1 - метры в мм\n2 - метры в см\n3 - метры в дм\n4 - метры в км")
+    if inp == '1':
+        break;
+    elif inp == '2':
+        break;
+    elif inp == '3':
+        break;
+    else:
+        break;
